86e02ab3f6c56973e52314d1675675cd03e57539
YaqiangCao <caoyaqiang0410@gmail.com>
2021-09-12 23:01:25 -0400

    update

--- 2.loops/3.H3K27ac_HiChIP/1.loops/run.sh
+++ 2.loops/3.H3K27ac_HiChIP/1.loops/run.sh
@@ -1,5 +1,6 @@
-#cLoops2 callLoops -d ../5.subSampling/GM12878_HiChIP_H3K27ac_150m -o GM12878_H3K27ac -eps 200,500,1000 -minPts 20 -w -j -cut 10000 -p 30
-#cLoops2 callLoops -d ../5.subSampling/K562_HiChIP_H3K27ac_150m -o K562_H3K27ac -eps 200,500,1000 -minPts 20 -w -j -cut 10000 -p 30
+#cLoops2 callLoops -d ../5.subSampling/GM12878_HiChIP_H3K27ac_150m -o GM12878_H3K27ac_1 -eps 1000,2000,3000,4000,5000 -minPts 10,20,30,40,50 -w -j -cut 10000 -p 10 &
 
-cLoops2 agg -d ../5.subSampling/GM12878_HiChIP_H3K27ac_150m -loops GM12878_H3K27ac_loops.txt -o GM12878_HiChIP_H3K27ac -p 30 -loop_norm
-cLoops2 agg -d ../5.subSampling/K562_HiChIP_H3K27ac_150m -loops K562_H3K27ac_loops.txt -o K562_HiChIP_H3K27ac -p 30 -loop_norm
+#cLoops2 callLoops -d ../5.subSampling/K562_HiChIP_H3K27ac_150m -o K562_H3K27ac_1 -eps 1000,2000,3000,4000,5000 -minPts 10,20,30,40,50 -w -j -cut 10000 -p 10 &
+
+cLoops2 agg -d ../5.subSampling/GM12878_HiChIP_H3K27ac_150m -loops GM12878_H3K27ac_1_loops.txt -o GM12878_HiChIP_H3K27ac -p 30 -loop_norm
+cLoops2 agg -d ../5.subSampling/K562_HiChIP_H3K27ac_150m -loops K562_H3K27ac_1_loops.txt -o K562_HiChIP_H3K27ac -p 30 -loop_norm
--- 2.loops/3.H3K27ac_HiChIP/3.diffLoopsAnnotation/run.sh
+++ 2.loops/3.H3K27ac_HiChIP/3.diffLoopsAnnotation/run.sh
@@ -1,4 +1,4 @@
-#cLoops2 anaLoops -loops gm_dloops.txt -gtf gencode.v30.basic.annotation.gtf -net -o gm -p 20 
-#cLoops2 anaLoops -loops k562_dloops.txt -gtf gencode.v30.basic.annotation.gtf -net -o k562 -p 20 
-#cLoops2 anaLoops -loops common_dloops.txt -gtf gencode.v30.basic.annotation.gtf -net -o common -p 20 
-cLoops2 anaLoops -loops ../../8.diffLoops/h3k27ac_dloops.txt -gtf gencode.v30.basic.annotation.gtf -net -o all -p 20 
+cLoops2 anaLoops -loops gm_dloops.txt -gtf gencode.v30.basic.annotation.gtf -net -o gm -p 20 
+cLoops2 anaLoops -loops k562_dloops.txt -gtf gencode.v30.basic.annotation.gtf -net -o k562 -p 20 
+cLoops2 anaLoops -loops common_dloops.txt -gtf gencode.v30.basic.annotation.gtf -net -o common -p 20 
+cLoops2 anaLoops -loops ../../8.1.diffLoops/h3k27ac_1_dloops.txt -gtf gencode.v30.basic.annotation.gtf -net -o all -p 20 
